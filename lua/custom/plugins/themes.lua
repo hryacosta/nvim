61a7daf454f75b7c961c84ac1b29289deca96ffc
Henry Acosta <henry@mftech.io>
2024-04-01 16:51:02 -0500

    core: themes

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -1,48 +1,18 @@
 return {
 
-  -- Or with configuration
   {
     'projekt0n/github-nvim-theme',
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('github-theme').setup {}
-      -- vim.cmd 'colorscheme github_dark'
-      -- vim.cmd 'colorscheme github_dark_default'
-      vim.cmd 'colorscheme github_dark_tritanopia'
+      -- vim.cmd 'colorscheme github_dark_tritanopia'
     end,
   },
   {
     'rose-pine/neovim',
     as = 'rose-pine',
     config = function()
-      -- vim.cmd 'colorscheme rose-pine'
-      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-    end,
-  },
-  {
-    'tjdevries/colorbuddy.nvim',
-    as = 'colorbuddy',
-    config = function()
-      -- vim.cmd 'colorscheme colorbuddy'
-      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-    end,
-  },
-  {
-    'Shatur/neovim-ayu',
-    config = function()
-      -- vim.cmd 'colorscheme ayu'
-    end,
-  },
-  {
-    'tiagovla/tokyodark.nvim',
-    opts = {
-      -- custom options here
-    },
-    config = function(_, opts)
-      require('tokyodark').setup(opts) -- calling setup is optional
-      -- vim.cmd [[colorscheme tokyodark]]
+      vim.cmd 'colorscheme rose-pine'
     end,
   },
 }
