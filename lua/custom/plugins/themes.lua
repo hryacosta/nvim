4443928e6e6ddcb79b957deb665657f5f05d317a
Henry Acosta <henry@mftech.io>
2024-04-01 17:00:37 -0500

    core: updates

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -12,6 +12,7 @@ return {
     'rose-pine/neovim',
     as = 'rose-pine',
     config = function()
+      require('rose-pine').setup {}
       vim.cmd 'colorscheme rose-pine'
       -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
       -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
