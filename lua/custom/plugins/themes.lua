bddc46b10ca926801ec64cda8c508c3b5d4353c2
Henry Acosta <henry@mftech.io>
2024-04-01 16:57:42 -0500

    core: updates

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -13,8 +13,8 @@ return {
     as = 'rose-pine',
     config = function()
       vim.cmd 'colorscheme rose-pine'
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
 }
