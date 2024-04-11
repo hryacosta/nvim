772195c902c679a6d1fc26f0c2652e6f07f7a732
Henry Acosta <henry@MacBook-Pro-de-Henry.local>
2024-04-11 10:14:11 -0500

    core: cmdline

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -13,7 +13,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('rose-pine').setup {}
-      -- vim.cmd 'colorscheme rose-pine'
+      vim.cmd 'colorscheme rose-pine'
       -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
       -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
