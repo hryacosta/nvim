23ccf19314066bbe9ca3f5f9bd4bee3c695d4945
Henry Acosta <henry@MacBook-Pro-de-Henry.local>
2024-04-15 19:54:16 -0500

    core: theme update

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -4,7 +4,7 @@ return {
     'projekt0n/github-nvim-theme',
     config = function()
       require('github-theme').setup {}
-      -- vim.cmd 'colorscheme github_dark_tritanopia'
+      vim.cmd 'colorscheme github_dark_default'
     end,
   },
   {
