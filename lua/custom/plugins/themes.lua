821e32b172253bcd5799fba877a7afd5322fb99f
Henry Acosta <henry@MacBook-Pro-de-Henry.local>
2024-04-17 09:54:43 -0500

    core: updates

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -4,9 +4,9 @@ return {
     'projekt0n/github-nvim-theme',
     config = function()
       require('github-theme').setup {}
-      vim.cmd 'colorscheme github_dark_default'
-      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      -- vim.cmd 'colorscheme github_dark_default'
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
   {
@@ -15,7 +15,17 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('rose-pine').setup {}
-      vim.cmd 'colorscheme rose-pine'
+      -- vim.cmd 'colorscheme rose-pine'
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    end,
+  },
+  {
+    'luisiacc/the-matrix.nvim',
+    config = function()
+      -- require('thematrix').setup {}
+
+      vim.cmd 'colorscheme thematrix'
       -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
       -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
