f0a2ecaf8a7c52e0e88d1b95e707b595b8e588c2
Henry Acosta <henry@MacBook-Pro-de-Henry.local>
2024-04-18 08:16:57 -0500

    core: updates

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -42,4 +42,21 @@ return {
       -- vim.cmd 'colorscheme thematrix'
     end,
   },
+  {
+    'rebelot/kanagawa.nvim',
+    config = function()
+      require('kanagawa').setup {
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = 'none',
+              },
+            },
+          },
+        },
+      }
+      vim.cmd 'colorscheme kanagawa-dragon'
+    end,
+  },
 }
