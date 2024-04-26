fb0f4aa64cbbe7f3f25396146da8989e0089fd76
Henry Acosta <henry@MacBook-Pro-de-Henry.local>
2024-04-26 16:49:59 -0500

    core: update recorgin

--- lua/custom/plugins/cmdline.lua
+++ lua/custom/plugins/cmdline.lua
@@ -22,6 +22,12 @@ return {
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
           lsp_doc_border = false, -- add a border to hover docs and signature help
         },
+        routes = {
+          {
+            view = 'notify',
+            filter = { event = 'msg_showmode' },
+          },
+        },
       }
     end,
   },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,24 +4,33 @@
 -- See the kickstart.nvim README for more information
 
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
 vim.keymap.set('n', '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save file' })
+
 vim.keymap.set('i', '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save file' })
+
 vim.keymap.set('n', '<leader>gg', vim.cmd.LazyGit, { desc = 'lazygit' })
+
 vim.keymap.set('n', '<leader>xx', function()
   require('trouble').toggle()
 end, { desc = 'trouble toggle' })
+
 vim.keymap.set('n', '<leader>xw', function()
   require('trouble').toggle 'workspace_diagnostics'
 end, { desc = 'toggle workspace diagnostics ' })
+
 vim.keymap.set('n', '<leader>xd', function()
   require('trouble').toggle 'document_diagnostics'
 end, { desc = 'trouble document diagnostics' })
+
 vim.keymap.set('n', '<leader>xq', function()
   require('trouble').toggle 'quickfix'
 end, { desc = 'trouble quickfix' })
+
 vim.keymap.set('n', '<leader>xl', function()
   require('trouble').toggle 'loclist'
 end, { desc = 'trouble loclist' })
+
 vim.keymap.set('n', 'gR', function()
   require('trouble').toggle 'lsp_references'
 end, { desc = 'trouble lsp references' })
