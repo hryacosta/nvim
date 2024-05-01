dd79d4451b5f4e2b8a84e3458de86fedbed33968
Henry Acosta <henry@MacBook-Pro-de-Henry.local>
2024-05-01 13:27:27 -0500

    core: theme

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -5,7 +5,9 @@ return {
     config = function()
       require('github-theme').setup {}
       -- vim.cmd 'colorscheme github_dark_default'
-      vim.cmd 'colorscheme github_dark_high_contrast'
+      -- vim.cmd 'colorscheme github_dark_high_contrast'
+      vim.cmd 'colorscheme github_dark_dimmed'
+      -- vim.cmd 'colorscheme github_dark'
       -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
       -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
