59b460a8cabe7fd11ad238a2b6e43c1f23d7b539
henry Acosta <henry@mftech.io>
2024-05-18 17:46:55 -0500

    core: transparent

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -2,15 +2,21 @@ return {
   {
     'projekt0n/github-nvim-theme',
     config = function()
-      require('github-theme').setup {}
+      require('github-theme').setup {
+        options = {
+          transparent = true,
+        },
+        styles = {
+          sidebars = 'transparent',
+          floats = 'transparent',
+        },
+      }
+
       -- vim.cmd 'colorscheme github_dark_default'
       -- vim.cmd 'colorscheme github_dark_high_contrast'
       -- vim.cmd 'colorscheme github_dark'
       vim.cmd 'colorscheme github_dark_colorblind'
       -- vim.cmd 'colorscheme github_dark_dimmed'
-
-      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
 }
