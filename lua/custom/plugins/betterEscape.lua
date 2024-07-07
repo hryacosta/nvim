49ff7597b965a66f94fb5845188dacb8d56669fb
Henry Acosta <henry@mftech.io>
2024-07-07 13:13:15 -0500

    core: updates

--- lua/custom/plugins/betterEscape.lua
+++ lua/custom/plugins/betterEscape.lua
@@ -1,6 +1,7 @@
 return {
   {
     'max397574/better-escape.nvim',
+    commit = '7e86edafb8c7e73699e0320f225464a298b96d12',
     config = function()
       require('better_escape').setup()
     end,
