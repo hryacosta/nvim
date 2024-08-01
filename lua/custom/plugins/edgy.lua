7dc67aa1a5b9b7819db640f8300c211a4b2f4d5b
Henry Acosta <henry@mftech.io>
2024-07-31 19:31:33 -0500

    core: updates

--- lua/custom/plugins/edgy.lua
+++ lua/custom/plugins/edgy.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    'folke/edgy.nvim',
-    event = 'VeryLazy',
-    init = function()
-      vim.opt.laststatus = 3
-      vim.opt.splitkeep = 'screen'
-    end,
-    opts = {},
-    config = function()
-      require('edgy').setup()
-    end,
-  },
+  -- {
+  --   'folke/edgy.nvim',
+  --   event = 'VeryLazy',
+  --   init = function()
+  --     vim.opt.laststatus = 3
+  --     vim.opt.splitkeep = 'screen'
+  --   end,
+  --   opts = {},
+  --   config = function()
+  --     require('edgy').setup()
+  --   end,
+  -- },
 }
