71f012d59dda718f216f0c36ba859a89860f113b
Henry Acosta <henry@mftech.io>
2024-08-01 13:09:16 -0500

    core: updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -118,18 +118,6 @@ wk.add {
 -- vim.opt.spell = true
 -- vim.opt.spelllang = { 'en', 'es' }
 
--- vim.api.nvim_create_autocmd('TermEnter', {
---   callback = function()
---     -- If the terminal window is lazygit, we do not make changes to avoid clashes
---     if string.find(vim.api.nvim_buf_get_name(0), 'lazygit') then
---       return
---     end
---
---     vim.keymap.set('t', '<ESC>', function()
---       vim.cmd 'stopinsert'
---     end, { buffer = true })
---   end,
--- })
 local group = vim.api.nvim_create_augroup('LazygitMods', { clear = true })
 vim.api.nvim_create_autocmd('TermEnter', {
   pattern = '*',
