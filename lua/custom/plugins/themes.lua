8c3bc143287db73ff55f02df725caaa3fd5e69e6
Henry Acosta <henry@mftech.io>
2024-08-20 14:05:54 -0500

    core: updates

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -59,28 +59,28 @@ return {
         compile = false, -- enable compiling the colorscheme
         undercurl = true, -- enable undercurls
         commentStyle = { italic = true },
-        functionStyle = {},
+        -- functionStyle = {},
         keywordStyle = { italic = true },
         statementStyle = { bold = true },
-        typeStyle = {},
+        -- typeStyle = {},
         transparent = false, -- do not set background color
         dimInactive = false, -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors) -- add/modify highlights
-          return {}
-        end,
-        theme = 'wave', -- Load "wave" theme when 'background' option is not set
+        -- colors = { -- add/modify theme and palette colors
+        --   palette = {},
+        --   theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        -- },
+        -- overrides = function(colors) -- add/modify highlights
+        --   return {}
+        -- end,
+        theme = 'lotus', -- Load "wave" theme when 'background' option is not set
         background = { -- map the value of 'background' option to a theme
           dark = 'wave', -- try "dragon" !
           light = 'lotus',
         },
       }
 
-      vim.cmd 'colorscheme kanagawa-dragon'
+      vim.cmd 'colorscheme kanagawa'
     end,
   },
 }
