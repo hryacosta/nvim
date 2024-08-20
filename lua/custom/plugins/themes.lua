872652e6e7881a375fd72f6068e6c4e193b9a58c
Henry Acosta <henry@mftech.io>
2024-08-20 12:43:55 -0500

    core updates

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -48,15 +48,39 @@ return {
         transparent_mode = false,
       }
 
-      vim.o.background = 'dark'
-      vim.cmd 'colorscheme gruvbox'
+      -- vim.o.background = 'dark'
+      -- vim.cmd 'colorscheme gruvbox'
     end,
   },
-  -- {
-  --   'savq/melange-nvim',
-  --   config = function()
-  --     -- vim.cmd 'set termguicolors'
-  --     -- vim.cmd 'colorscheme melange'
-  --   end,
-  -- },
+  {
+    'rebelot/kanagawa.nvim',
+    config = function()
+      require('kanagawa').setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = 'wave', -- Load "wave" theme when 'background' option is not set
+        background = { -- map the value of 'background' option to a theme
+          dark = 'wave', -- try "dragon" !
+          light = 'lotus',
+        },
+      }
+
+      vim.cmd 'colorscheme kanagawa-dragon'
+    end,
+  },
 }
