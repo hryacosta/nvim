5efb488d1ac48007bec3a022076eef17b5643230
Henry Acosta <henry@mftech.io>
2024-08-20 15:37:23 -0500

    core: updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -96,7 +96,7 @@ wk.add {
     hidden = true,
   },
 }
--- vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
 
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*',
