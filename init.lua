7800e7c6b0629892fe10a067868fc635f94d04a0
Henry Acosta <henry@mftech.io>
2024-08-23 02:29:39 -0500

    core: show when recoding macro

--- init.lua
+++ init.lua
@@ -1125,7 +1125,57 @@ require('lazy').setup({
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup {
+        use_icons = vim.g.have_nerd_font,
+        content = {
+          active = function()
+            local check_macro_recording = function()
+              if vim.fn.reg_recording() ~= '' then
+                return 'Recording @' .. vim.fn.reg_recording()
+              else
+                return ''
+              end
+            end
+
+            local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+            local git = MiniStatusline.section_git { trunc_width = 40 }
+            local diff = MiniStatusline.section_diff { trunc_width = 75 }
+            local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+            -- local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+            local filename = MiniStatusline.section_filename { trunc_width = 140 }
+            local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+            local location = MiniStatusline.section_location { trunc_width = 200 }
+            local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+            local macro = check_macro_recording()
+
+            return MiniStatusline.combine_groups {
+              { hl = mode_hl, strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineFilename', strings = { macro } },
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
+            }
+          end,
+        },
+      }
+
+      vim.api.nvim_create_autocmd('RecordingEnter', {
+        pattern = '*',
+        callback = function()
+          vim.cmd 'redrawstatus'
+        end,
+      })
+
+      -- Autocmd to track the end of macro recording
+      vim.api.nvim_create_autocmd('RecordingLeave', {
+        pattern = '*',
+        callback = function()
+          vim.cmd 'redrawstatus'
+        end,
+      })
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
