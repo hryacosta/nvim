3d577173311716335eac4515f21b64fe49fd04d7
Henry Acosta <henry@mftech.io>
2025-04-01 09:25:22 -0500

    chore: remove tabine and add copilot
    Signed-off-by: Henry Acosta <henry@mftech.io>

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,36 +1,52 @@
 return {
 
-  -- {
-  --   'zbirenbaum/copilot.lua',
-  --   event = 'InsertEnter',
-  --   cmd = 'Copilot',
-  --   build = ':Copilot auth',
-  --   opts = {
-  --     panel = { enabled = false },
-  --     suggestion = { enabled = false },
-  --     filetypes = {
-  --       markdown = true,
-  --       help = true,
-  --     },
-  --   },
-  -- },
-  -- {
-  --   'zbirenbaum/copilot-cmp',
-  --   opts = {},
-  --   config = function(_, opts)
-  --     local copilot_cmp = require 'copilot_cmp'
-  --     copilot_cmp.setup(opts)
-  --   end,
-  -- },
-  -- {
-  --   'CopilotC-Nvim/CopilotChat.nvim',
-  --   branch = 'canary',
-  --   dependencies = {
-  --     { 'zbirenbaum/copilot.lua' },
-  --     { 'nvim-lua/plenary.nvim' },
-  --   },
-  --   config = function()
-  --     require('CopilotChat').setup()
-  --   end,
-  -- },
+  {
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter',
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    opts = {
+      panel = { enabled = false, auto_refresh = true },
+      suggestion = { enabled = true, auto_trigger = true },
+      filetypes = {
+        markdown = true,
+        help = true,
+        typescript = true,
+        javascript = true,
+        rust = true,
+        go = true,
+        dart = true,
+      },
+    },
+    config = function(opts)
+      require('copilot').setup(opts)
+    end,
+  },
+  {
+    'zbirenbaum/copilot-cmp',
+    opts = {},
+    config = function(_, opts)
+      local copilot_cmp = require 'copilot_cmp'
+      copilot_cmp.setup {
+        sources = {
+          -- Copilot Source
+          { name = 'copilot', group_index = 2 },
+          -- Other Sources
+          { name = 'nvim_lsp', group_index = 2 },
+          { name = 'path', group_index = 2 },
+          { name = 'luasnip', group_index = 2 },
+        },
+      }
+    end,
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' },
+      { 'nvim-lua/plenary.nvim' },
+    },
+    config = function()
+      require('CopilotChat').setup()
+    end,
+  },
 }
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -128,14 +128,15 @@ vim.api.nvim_create_autocmd('TermEnter', {
 })
 
 --- Integration with Tabnine and LuaSnip; falling back to inserting tab if neither has a completion
-vim.keymap.set('i', '<tab>', function()
-  if require('tabnine.keymaps').has_suggestion() then
-    return require('tabnine.keymaps').accept_suggestion()
-  elseif require('luasnip').jumpable(1) then
-    return require('luasnip').jump(1)
-  else
-    return '<tab>'
-  end
-end, { expr = true })
+-- vim.keymap.set('i', '<tab>', function()
+--   if require('tabnine.keymaps').has_suggestion() then
+--     return require('tabnine.keymaps').accept_suggestion()
+--   elseif require('luasnip').jumpable(1) then
+--     return require('luasnip').jump(1)
+--   else
+--     return '<tab>'
+--   end
+-- end, { expr = true })
+--
 
 return {}
